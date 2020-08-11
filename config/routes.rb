76bbaf9ed5a9c94c907e830e1598152b9dbc3b7f
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-08-11 07:57:12 -0700

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :blogs
-      # get 'blogs/index'
-      post 'blogs/create'
-      # get '/show/:id', to: 'recipes#show'
-      # delete '/destroy/:id', to: 'recipes#destroy'
+      get '/blogs', to: 'blogs#index'
+      get '/blogs/new', to: 'blogs#new'
+      post 'blogs/create', to: 'blogs#create'
+      get '/blogs/edit/:id', to: 'blogs#edit'
+      put '/blogs/update/:id', to: 'blogs#update'
+      delete '/blogs/destroy/:id', to: 'blogs#destroy'
     end
   end
   root 'home#index'
