3378473a0c55bcbde31ee3f22f3baac37dcccfb6
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-08-10 18:21:51 -0700

    create blogs controller

---
+++ app/controllers/api/v1/blogs_controller.rb
@@ -0,0 +1,47 @@
+class Api::V1::BlogsController < ApplicationController
+    before_action :find_blog, except: [:index, :new, :create]
+
+    def index
+        @blogs = Blog.all.order(created_at: :desc)
+        render json: @blogs
+    end
+
+    def show
+        if @recipe
+            render json: @recipe
+        else
+            render json: @recipe.errors
+        end
+    end
+
+    def new
+        @blog = Blog.new
+    end
+
+    def create
+        @blog = Blog.create!(blog_params)
+        if @blog
+            render json: @blog
+        else
+            render json: @blog.errors
+        end
+    end
+
+    def delete
+        if @blog.destroy
+            render json: { message: 'Blog deleted!' }
+        else
+            render json: @blog.error
+        end
+    end
+
+    private
+
+    def find_blog
+        @blog = Blog.find(params[:id])
+    end
+
+    def blog_params
+        params.require(:blog).permit(:title, :author, :content, :image)
+    end
+end
